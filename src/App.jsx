9d7452e75728dbbbd137b0cf21d8778799ad3bb8
arunampire790 <arunampire@gamil.com>
2026-08-12 22:27:01 +0530

    Reduce App to minimal application shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { motion } from 'motion/react'
-import { useReducedMotion } from './hooks/useReducedMotion'
-
 function App() {
-  useReducedMotion()
-
-  return (
-    <main className="min-h-svh">
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} aria-hidden="true" />
-    </main>
-  )
+  return <main className="min-h-svh" />
 }
 
 export default App
